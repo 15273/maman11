ff55f8317bfdefcbdc01f36825aef56853989221
מנחם מנדל קליש <mnhmmndlqlys@mnhms-macbook-pro.local>
2023-12-06 22:04:59 +0200

    Initial commit

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 int main() {
     double dollar, number = 0;
-    printf("please enter numbers separated by spaces / tabs:\n");
+    printf("please enter numbers separated by spaces / tabs if you want the program to stop pres q:\n");
     scanf("%lf", &dollar);
     printf("\n\t$\t\t\t\tNIS\t\n");
     while (scanf("%lf", &number) == 1) {
