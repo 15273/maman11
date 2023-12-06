ba768d3ea835fe183f30f2a56b05a271a0ff0076
מנחם מנדל קליש <mnhmmndlqlys@mnhms-macbook-pro.local>
2023-12-06 22:04:59 +0200

    Initial commit

--- main.c
+++ main.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main() {
-    int count = 0;
-    int i = 0;
-    double dolar;
-    char *token, input[1024];
+    int i = 1;
+    float dollar, number = 0;
 
     printf("Enter numbers separated by spaces, tabs, or newlines:\n");
+    scanf("%f", &dollar);
 
-    fgets(input, sizeof(input), stdin); /* Read input from user */
-
-    printf("the values that you enter is: %s\n", input);
-    token = strtok(input, " \t\n"); /* Tokenize using space, tab, and newline as delimiters */
-    printf("Index\t Value\t Shekels \n");
-    dolar = atof(token);
-    while (token != NULL) {
-        /* Allocate memory for the numbers array dynamically */
-        count++;
-        token = strtok(NULL, " \t");
-        printf("%d\t%.2f\t%.2f\n", (1 + i), atof(token), (atof(token) * dolar));
+    printf("Index\tValue\tShekels\n");
+    while (scanf("%f", &number) == 1) {
+        printf("%i\t%f\t%f\n", i, number, number * dollar);
         i++;
     }
+
     return 0;
 }
-
