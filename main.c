a1fed724793208101d1d1f54d4dccf438bbb5a5c
מנחם מנדל קליש <mnhmmndlqlys@mnhms-macbook-pro.local>
2023-12-06 22:04:59 +0200

    Initial commit

--- main.c
+++ main.c
@@ -2,16 +2,14 @@
 
 int main() {
     int i = 1;
-    float dollar, number = 0;
-
+    double dollar, number = 0;
     printf("Enter numbers separated by spaces, tabs, or newlines:\n");
-    scanf("%f", &dollar);
-
-    printf("Index\tValue\tShekels\n");
-    while (scanf("%f", &number) == 1) {
-        printf("%i\t%f\t%f\n", i, number, number * dollar);
+    scanf("%lf", &dollar);
+    printf("Index\t\t$\t\tNIS\n");
+    while (scanf("%lf", &number) == 1) {
+        printf("%i\t\t%.2lf\t\t%.2lf\n", i, number * dollar, number);
         i++;
     }
-
     return 0;
 }
+
