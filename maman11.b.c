98e3d1e5fe5f99f51ef1201d7a9fcf8294efedb6
מנחם מנדל קליש <mnhmmndlqlys@mnhms-macbook-pro.local>
2023-12-06 22:04:59 +0200

    Initial commit

--- maman11.b.c
+++ maman11.b.c
@@ -1,75 +1,55 @@
-//
-// Created by מנחם מנדל קליש on 06/12/2023.
-//
+#include <stdio.h>
+#include <math.h>
 
-#include "maman11.b.h"
-double absolute(double num) {
-    if (num < 0) {
-        return -num;
-    } else {
-        return num;
-    }
+double pos(double x) {
+    if (x < 0)
+        return -x;
+    return x;
 }
 
-/* calculate one step */
-float oneStepInTheCalculator(int tow_jump, double x) {
-    int i;
-    float factorial = (float) tow_jump;
-    float number = (float) x;
-    for (i = 1; i < tow_jump; i++) {
-        factorial = factorial * (float) (tow_jump - (i));
-        number = (float) x * number;
+double reduce_angle(double angle) {
+    // Reduce angle to the range [-pi, pi]
+    while (angle > 3.141592653589793) {
+        angle -= 2 * 3.141592653589793;
     }
-    number = (float) (number / ((float) factorial));
-    return number;
+    while (angle < -3.141592653589793) {
+        angle += 2 * 3.141592653589793;
+    }
+    return angle;
 }
 
-/* the main method that calculate the cosine */
-double double_my_cos(double x_to_cos) {
-    double min_size_to_stop = 0.000001;
-    float next_number = 1, cosine = 0, final_score_should_be = cos(x_to_cos);
-    int i = 0, tow_jump = 0, flag = 1, pos_or_neg = 1;
-    /*
-     * the main loop work until the next step is less then the minimum value = 0.000001
-     * */
-    while (flag == 1) {
-        cosine += (float) pos_or_neg * next_number;
-        pos_or_neg *= -1;
-        tow_jump += 2;
-        i += 1;
-        if (next_number < min_size_to_stop || absolute((final_score_should_be - cosine)) < min_size_to_stop)
-            flag = 0;
-        next_number = oneStepInTheCalculator(tow_jump, x_to_cos);
+int fact(int n) {
+    int factorial = 1;
+    for (int i = 2; i <= n; ++i) {
+        factorial *= i;
     }
-    /*
-     * comparing between are result and the built in cos function of c
-     * */
-    printf("the result of aur method is: %f\n", cosine);
-    printf("and the result of the built in c method is:\t %f\n", cos(x_to_cos));
-    return cosine;
+    return factorial;
 }
+
+double cos_taylor(double x) {
+    x = reduce_angle(x);  // Reduce the angle within the range [-pi, pi]
+    double result = 1.0;  // Initialize result with the first term in the series
+    double power = 1.0, next = 1.0;
+    int sign = -1;
+    int n = 2;
+    while (pos(next) > 0.000001) {
+        power *= x * x;  // Update the power term
+        next = (sign * power / (double) fact(n));
+        result += next;  // Calculate the next term in the series
+        sign *= -1;  // Alternate the sign
+        n += 2;
+    }
+    return result;
+}
+
 
 int main() {
-    double my_cos, radians;
-    printf("please enter a number that you want to calculate the cosine for him: ");
-    scanf("%lf", &my_cos);
-
-//    radians = my_cos * (M_PI / 180.0);
-//    double_my_cos(radians);
-
-    /*
-     * convert degrees to radians
-     * in case that i get the numbers as a degrees
-     * i will convert him and then call the method with the radians
-     * */
-    radians = my_cos * (M_PI / 180.0);
-    printf("the first call to the method\n");
-    double_my_cos(radians);
-    /*
-     * in case that the number is already radians
-     *
-     * (the option that the number is a regular number not exist)
-     * */
-    printf("the second call to the method\n");
-    double_my_cos(my_cos);
+    double angle;
+    printf("Please enter an angle (in radians) to calculate its cosine: ");
+    scanf("%lf", &angle);
+    int terms = 10;  // Number of terms in the Taylor series expansion
+    printf("the built in function of c return : %f\n", cos(angle));
+    double cosine = cos_taylor(angle);
+    printf("Cosine of %.2f radians in are method is: %.6f\n", angle, cosine);
+    return 0;
 }
--- maman11.b.h
+++ maman11.b.h
@@ -8,16 +8,26 @@
 #include <stdio.h>
 #include <math.h>
 
+double absolute(double num) {
+    if (num < 0) {
+        return -num;
+    } else {
+        return num;
+    }
+}
 
-#include <limits.h>
-
-
-
-#include <limits.h>
-
-
-#include <limits.h>
-
+/* calculate one step */
+float oneStepInTheCalculator(int tow_jump, double x) {
+    int i;
+    float factorial = (float) tow_jump;
+    float number = (float) x;
+    for (i = 1; i < tow_jump; i++) {
+        factorial = factorial * (float) (tow_jump - (i));
+        number = (float) x * number;
+    }
+    number = (float) (number / ((float) factorial));
+    return number;
+}
 
 
 #endif //UNTITLED2_MAMAN11_B_H
